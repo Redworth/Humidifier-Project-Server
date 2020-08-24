872690395cce97371b08469cff45eed58d7494f4
rohit-karthik <rohit.karthik200@gmail.com>
2020-08-24 12:08:14 -0700

    mods

---
+++ apiAppSignIn.py
@@ -0,0 +1,16 @@
+from apiApp1 import app
+
+@app.route('/app-request/SignIn', methods=['GET', 'POST'])
+def appRequestON():
+    req_data = request.get_json()
+    
+    try:
+        username = req_data['username']
+        password = req_data['password']
+
+    except:
+        username = "Not found"
+        password = "Not found"
+    
+    return """Username: {}
+              Password: {}""".format(username, password)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from apiApp import app
+from apiApp1 import app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8000')
