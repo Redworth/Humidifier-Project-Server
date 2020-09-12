19acb2a663c93b8e7f27ec4d7ea8bf5ece0a4d04
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2020-09-12 14:31:36 -0700

    Update requestCheck.py

--- requestCheck.py
+++ requestCheck.py
@@ -1,7 +1,28 @@
-username = input("Enter username: ")
-targetdevice = input("Enter Device: ")
-requestdetails = input('Enter details: ')
-hum_intensity = input("Enter intensity of the humidifier")
+#Because most user info will be stored in a database and not as inputs, it makes more sense to use a dictionary as a temporary storage space instead of input.
+#So, I am updating your files with the following. I will comment out your input statements for now.
+
+users_temp_dict = {
+    "rohit" : {
+        "devices" : ["HUM1", "Rohit's Humidifier"]
+    }
+    "arav" : {
+        "devices" : ["HUM1", "Arav's Humidifier"]
+    }
+    "tal" : {
+        "devices" : ["HUM1", "Tal's Humidifier"]
+    }
+    "ethan" : {
+        "devices" : ["HUM1", "Ethan's Humidifier"]
+    }
+    "ishaan" : {
+        "devices" : ["HUM1", "Ishaan's Humidifier"]
+    }
+}
+
+"""username = input("Enter username: ")
+   targetdevice = input("Enter Device: ")
+   requestdetails = input('Enter details: ')
+   hum_intensity = input("Enter intensity of the humidifier")"""
 
 def requestCheck(dict_check):
     while True:
