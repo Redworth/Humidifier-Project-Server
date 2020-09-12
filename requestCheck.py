2eef34f782d7c2bc017b5ce3e4096d75f1face9c
Arav <aravbhosle@gmail.com>
2020-09-12 13:32:54 -0700

    Update requestCheck.py

--- requestCheck.py
+++ requestCheck.py
@@ -1,1 +1,28 @@
-print("test")
+username = input("Enter username: ")
+targetdevice = input("Enter Device: ")
+requestdetails = input('Enter details: ')
+hum_intensity = input("Enter intensity of the humidifier")
+
+def requestCheck(dict_check):
+    while True:
+        if username in  dict_check['username']:
+            break
+        else
+            print("Not Valid Username. Try Again.")
+
+    while True:
+        if targetdevice in dict_check['targetDevice']:
+            break
+        else
+            print("Not Valid Device Name. Try Again.")
+    while True:
+        if requestdetails in  dict_check['requestDetails']:
+            break
+        else
+            print("Not Valid Details. Try Again.")
+    while True:
+        if 0 <= int(hum_intensity) <= 100 or hum_intensity == 'NA':
+            break
+        else
+            print("Not Valid Intensity. Try Again.")
+
