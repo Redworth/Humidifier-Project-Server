b575e832fe2c823a20e1e28fb1ab8898ff8669e6
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2021-01-12 15:43:41 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='redworthapp',
-    version='1.0.4',
+    version='1.0.5',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
