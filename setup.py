9f58a9a311f482937309910f34502a8bc6deffa1
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2021-01-12 19:39:00 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='redworthapp',
     version='1.0.9',
     packages=find_packages(),
+    py_modules=['redworthapp.apiApp1', 'redworthapp.apiApp2', 'redworthapp.apiAppSignIn', 'redworthapp.requestCheck', 'redworthapp.databaseConnection', 'redworthapp.databaseConnectionTest'],
     zip_safe=False,
     install_requires=[
         'flask',
