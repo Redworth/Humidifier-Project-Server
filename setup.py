d14884ceb4b52d4c10f2b6ee8b47703373e40de4
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2021-01-16 13:27:29 -0800

    update files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import versioneer
 
 setup(
     name='redworthapp',
-    version='1.1.1',
+    version='1.1.2',
     packages=find_packages(),
     py_modules=['redworthapp.apiApp1', 'redworthapp.apiApp2', 'redworthapp.apiAppSignIn', 'redworthapp.requestCheck', 'redworthapp.databaseConnection', 'redworthapp.databaseConnectionTest'],
     zip_safe=False,
