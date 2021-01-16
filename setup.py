a0168ba81d70d73660859ec45ab770199e0fd3ee
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2021-01-16 13:38:28 -0800

    Bump version: 1.1.3 → 1.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import versioneer
 
 setup(
     name='redworthapp',
-    version='1.1.3',
+    version='1.1.4',
     packages=find_packages(),
     py_modules=['redworthapp.apiApp1', 'redworthapp.apiApp2', 'redworthapp.apiAppSignIn', 'redworthapp.requestCheck', 'redworthapp.databaseConnection', 'redworthapp.databaseConnectionTest'],
     zip_safe=False,
