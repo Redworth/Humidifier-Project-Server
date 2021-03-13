b6684fb8faf451129ba4a12449ada87927aa50c5
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2021-03-13 15:24:46 -0800

    Update apiApp1.py

--- apiApp1.py
+++ apiApp1.py
@@ -2,8 +2,10 @@ from flask import Flask, request
 import json
 import requestCheck
 import databaseConnection
+from flask_cors import *
 
 app = Flask(__name__)
+CORS(app)
 
 import apiAppIOT
 import apiAppSignIn
