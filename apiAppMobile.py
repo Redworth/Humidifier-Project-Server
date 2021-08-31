3b411e7e740d91f886724b2d4c0266da5abaf5de
rohit-karthik <63020497+rohit-karthik@users.noreply.github.com>
2021-08-31 14:45:17 -0700

    finish retrieving device details

--- apiAppMobile.py
+++ apiAppMobile.py
@@ -104,3 +104,28 @@ def getDevicesInfo():
         }
 
     return jsonReturn
+
+@app.route('/specific-device', methods=['POST'])
+def appRequestOneDevice():
+    try:
+        req_data = request.get_json()
+
+        check = requestCheck.requestCheckPoll(req_data)
+
+        if check['Result'] == "Success":
+            returnDict = {
+                "intensity": ""
+            }
+            newVal = databaseConnection.getData(req_data['username'], req_data['targetDevice'])
+            returnDict['intensity'] = newVal
+
+            return json.dumps(returnDict)
+        
+        return {
+            "Result": "Failure"
+        }
+
+    except:
+        return {
+            "Result": "Failure"
+        }
